5aea34de27c58d429b6127b37f7f3fe1490e85ed
Schuberty <schubertyi@gmail.com>
2021-08-09 12:49:02 -0300

    Removed unnecessary package

--- src/main/java/io/schuberty/dio/personaapi/system/service/PersonService.java
+++ src/main/java/io/schuberty/dio/personaapi/system/service/PersonService.java
@@ -3,7 +3,6 @@ package io.schuberty.dio.personaapi.system.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
