18c6b250ca68f1063bce4dcbe9bcbb62377205e7
Schuberty <schubertyi@gmail.com>
2021-08-09 13:07:18 -0300

    Added Person POST, GET, PUT and DELETE mapping

--- src/main/java/io/schuberty/dio/personaapi/system/controller/PersonController.java
+++ src/main/java/io/schuberty/dio/personaapi/system/controller/PersonController.java
@@ -1,9 +1,24 @@
 package io.schuberty.dio.personaapi.system.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.schuberty.dio.personaapi.exception.PersonNotFoundException;
+import io.schuberty.dio.personaapi.system.dto.request.PersonDTO;
+import io.schuberty.dio.personaapi.system.dto.response.MessageResponseDTO;
 import io.schuberty.dio.personaapi.system.service.PersonService;
 import lombok.AllArgsConstructor;
 
@@ -13,4 +28,31 @@ import lombok.AllArgsConstructor;
 public class PersonController {
 
     private PersonService personService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return this.personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return this.personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable("id") Long id) throws PersonNotFoundException {
+        return this.personService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable("id") Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return this.personService.updateById(id, personDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MessageResponseDTO deleteById(@PathVariable("id") Long id) throws PersonNotFoundException {
+        return this.personService.delete(id);
+    }
 }
