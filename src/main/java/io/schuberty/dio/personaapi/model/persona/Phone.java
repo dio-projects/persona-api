0c4a3d31c39d3af70f5b2d6dc76a6a07ff32135f
Schuberty <schubertyi@gmail.com>
2021-08-09 13:03:53 -0300

    Fixed Phone ID missing

--- src/main/java/io/schuberty/dio/personaapi/model/persona/Phone.java
+++ src/main/java/io/schuberty/dio/personaapi/model/persona/Phone.java
@@ -4,6 +4,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import io.schuberty.dio.personaapi.enums.PhoneType;
 import lombok.AllArgsConstructor;
@@ -19,6 +22,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
