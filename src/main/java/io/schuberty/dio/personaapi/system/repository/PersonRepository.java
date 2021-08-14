2848618e49b0d31640b44d81f8a0639526f784c9
Schuberty <schubertyi@gmail.com>
2021-08-14 17:21:44 -0300

    Fixed repository annotation missing

--- src/main/java/io/schuberty/dio/personaapi/system/repository/PersonRepository.java
+++ src/main/java/io/schuberty/dio/personaapi/system/repository/PersonRepository.java
@@ -1,7 +1,9 @@
 package io.schuberty.dio.personaapi.system.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import io.schuberty.dio.personaapi.model.persona.Person;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> { }
